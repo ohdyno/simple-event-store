dec5ea98e0c6eed210a96e450320b7284931ac6a
Xing Zhou <xing@xingzhou.me>
2023-10-28 13:00:54 -0400

    chore: initialize project with standard Kotlin structure

---
+++ lib/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.9.10"
+    id("java-library")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.3")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // This dependency is exported to consumers, that is to say found on their compile classpath.
+    api("org.apache.commons:commons-math3:3.6.1")
+
+    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
+    implementation("com.google.guava:guava:32.1.1-jre")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
