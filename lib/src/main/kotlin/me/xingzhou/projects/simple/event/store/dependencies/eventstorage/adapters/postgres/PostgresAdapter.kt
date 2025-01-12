6881bc73054cea62c6542f2f891f5326a198d5c5
Xing Zhou <xing@xingzhou.me>
2025-01-12 00:07:38 -0500

    refactor(postgres-adapter): simplify long methods

--- lib/src/main/kotlin/me/xingzhou/projects/simple/event/store/dependencies/eventstorage/adapters/postgres/PostgresAdapter.kt
+++ lib/src/main/kotlin/me/xingzhou/projects/simple/event/store/dependencies/eventstorage/adapters/postgres/PostgresAdapter.kt
@@ -2,6 +2,7 @@ package me.xingzhou.projects.simple.event.store.dependencies.eventstorage.adapte
 
 import java.sql.Connection
 import java.sql.PreparedStatement
+import java.sql.ResultSet
 import java.sql.SQLException
 import java.sql.Timestamp
 import java.time.Instant
@@ -27,49 +28,6 @@ fun setupDatabase(connection: Connection) {
   }
 }
 
-internal class EventSourceSql {
-  object Query {
-    const val INSERT_EVENT =
-        """INSERT INTO ${Tables.EVENTS} (${Columns.STREAM_NAME}, ${Columns.VERSION}, ${Columns.EVENT_ID}, ${Columns.EVENT_TYPE}, ${Columns.EVENT_DATA}, ${Columns.OCCURRED_ON})
-           VALUES                       (           ?,                      ?,                  ?,                  ?,                  (?::jsonb),                 ?         );"""
-    const val RETRIEVE_STREAM_WITH_FILTER =
-        """SELECT * FROM ${Tables.EVENTS} 
-           WHERE ${Columns.STREAM_NAME} = ?
-           AND ${Columns.EVENT_TYPE} = ANY (?)
-           ORDER BY ${Columns.VERSION};"""
-    const val RETRIEVE_STREAM =
-        """SELECT * FROM ${Tables.EVENTS}
-           WHERE ${Columns.STREAM_NAME} = ?
-           ORDER BY ${Columns.VERSION};"""
-    const val RETRIEVE_SYSTEM = "SELECT * FROM ${Tables.EVENTS} ORDER BY ${Columns.OCCURRED_ON};"
-    const val RETRIEVE_SYSTEM_WITH_FILTER =
-        """SELECT * FROM ${Tables.EVENTS}
-        WHERE ${Columns.EVENT_TYPE} = ANY (?)
-        ORDER BY ${Columns.OCCURRED_ON};"""
-    const val STREAM_EXISTS = "SELECT 1 FROM ${Tables.EVENTS} WHERE ${Columns.STREAM_NAME} = ?;"
-    const val RETRIEVE_APPEND_TOKEN =
-        "SELECT ${Columns.APPEND_TOKEN} FROM ${Tables.APPEND_TOKENS} WHERE ${Columns.STREAM_NAME} = ?;"
-    const val RETRIEVE_SYSTEM_TIMESTAMP =
-        "SELECT COALESCE(MAX(${Columns.TIMESTAMP}), CURRENT_TIMESTAMP) AS timestamp FROM ${Tables.EVENTS};"
-  }
-
-  object Tables {
-    const val EVENTS = "eventsource.events"
-    const val APPEND_TOKENS = "eventsource.append_tokens"
-  }
-
-  object Columns {
-    const val STREAM_NAME = "stream_name"
-    const val VERSION = "version"
-    const val EVENT_ID = "event_id"
-    const val EVENT_TYPE = "event_type"
-    const val EVENT_DATA = "event_data"
-    const val OCCURRED_ON = "occurred_on"
-    const val APPEND_TOKEN = "append_token"
-    const val TIMESTAMP = "timestamp"
-  }
-}
-
 internal class PostgresAdapter(internal val dataSource: DataSource) : ForEventStorage {
   override fun createStream(
       streamName: String,
@@ -78,27 +36,26 @@ internal class PostgresAdapter(internal val dataSource: DataSource) : ForEventSt
       eventData: String,
       occurredOn: Instant
   ): String {
-    return dataSource.connection.use {
-      it.runCatching {
-            prepareStatement(EventSourceSql.Query.INSERT_EVENT)
-                .apply {
-                  setString(1, streamName)
-                  setInt(2, 0)
-                  setString(3, eventId)
-                  setString(4, eventType)
-                  setString(5, eventData)
-                  setTimestamp(6, Timestamp.from(occurredOn))
-                }
+    return runCatching {
+          dataSource.connection.use { connection ->
+            prepareInsertQuery(
+                    connection = connection,
+                    streamName = streamName,
+                    version = 0,
+                    eventId = eventId,
+                    eventType = eventType,
+                    eventData = eventData,
+                    occurredOn = occurredOn)
                 .run { execute() }
                 .let { 1.toString() }
           }
-          .getOrElse {
-            throw when {
-              it.isUniqueConstraintViolation() -> StreamAlreadyExists(name = streamName)
-              else -> it
-            }
+        }
+        .getOrElse {
+          throw when {
+            it.isUniqueConstraintViolation() -> StreamAlreadyExists(name = streamName)
+            else -> it
           }
-    }
+        }
   }
 
   override fun appendToStream(
@@ -109,32 +66,49 @@ internal class PostgresAdapter(internal val dataSource: DataSource) : ForEventSt
       eventData: String,
       occurredOn: Instant
   ): String {
-    return dataSource.connection.use {
-      it.runCatching {
+    return runCatching {
+          dataSource.connection.use { connection ->
             appendToken.toInt().let { version ->
-              prepareStatement(EventSourceSql.Query.INSERT_EVENT)
-                  .apply {
-                    setString(1, streamName)
-                    setInt(2, version)
-                    setString(3, eventId)
-                    setString(4, eventType)
-                    setString(5, eventData)
-                    setTimestamp(6, Timestamp.from(occurredOn))
-                  }
+              prepareInsertQuery(
+                      connection = connection,
+                      streamName = streamName,
+                      version = version,
+                      eventId = eventId,
+                      eventType = eventType,
+                      eventData = eventData,
+                      occurredOn = occurredOn)
                   .run { execute() }
                   .let { version.inc().toString() }
             }
           }
-          .getOrElse {
-            throw when {
-              it.isUniqueConstraintViolation() || it is NumberFormatException ->
-                  ForEventStorage.Failure.InvalidAppendToken(
-                      streamName = streamName, appendToken = appendToken)
-              else -> it
-            }
+        }
+        .getOrElse {
+          throw when {
+            it.isUniqueConstraintViolation() || it is NumberFormatException ->
+                ForEventStorage.Failure.InvalidAppendToken(
+                    streamName = streamName, appendToken = appendToken)
+            else -> it
           }
-    }
+        }
   }
+
+  private fun prepareInsertQuery(
+      connection: Connection,
+      streamName: String,
+      version: Int,
+      eventId: String,
+      eventType: String,
+      eventData: String,
+      occurredOn: Instant
+  ): PreparedStatement =
+      connection.prepareStatement(EventSourceSql.Query.INSERT_EVENT).apply {
+        setString(1, streamName)
+        setInt(2, version)
+        setString(3, eventId)
+        setString(4, eventType)
+        setString(5, eventData)
+        setTimestamp(6, Timestamp.from(occurredOn))
+      }
 
   override fun retrieveFromStream(streamName: String, eventTypes: List<String>): StreamEvents {
     return dataSource.connection.use { connection ->
@@ -151,20 +125,10 @@ internal class PostgresAdapter(internal val dataSource: DataSource) : ForEventSt
       streamName: String,
       eventTypes: List<String>
   ): List<StreamEvent> =
-      prepareRetrieveQuery(connection, streamName, eventTypes)
+      prepareRetrieveQuery(
+              connection = connection, streamName = streamName, eventTypes = eventTypes)
           .run { executeQuery() }
-          .let {
-            buildList {
-              while (it.next()) {
-                add(
-                    StreamEvent(
-                        eventType = it.getString(EventSourceSql.Columns.EVENT_TYPE),
-                        eventData = it.getString(EventSourceSql.Columns.EVENT_DATA),
-                        occurredOn =
-                            it.getTimestamp(EventSourceSql.Columns.OCCURRED_ON).toInstant()))
-              }
-            }
-          }
+          .run { extractStreamEvents() }
 
   private fun prepareRetrieveQuery(
       connection: Connection,
@@ -187,26 +151,11 @@ internal class PostgresAdapter(internal val dataSource: DataSource) : ForEventSt
 
   override fun retrieveFromSystem(eventTypes: List<String>): SystemEvents {
     return dataSource.connection.use { connection ->
-      retrieveSystemTimestamp(connection).let { timestamp ->
-        prepareRetrieveQuery(connection, eventTypes)
+      retrieveSystemTimestamp(connection = connection).let { timestamp ->
+        prepareRetrieveQuery(connection = connection, eventTypes = eventTypes)
             .run { executeQuery() }
-            .let {
-              buildList {
-                    while (it.next()) {
-                      add(
-                          SystemEvent(
-                              streamName = it.getString(EventSourceSql.Columns.STREAM_NAME),
-                              streamEvent =
-                                  StreamEvent(
-                                      eventType = it.getString(EventSourceSql.Columns.EVENT_TYPE),
-                                      eventData = it.getString(EventSourceSql.Columns.EVENT_DATA),
-                                      occurredOn =
-                                          it.getTimestamp(EventSourceSql.Columns.OCCURRED_ON)
-                                              .toInstant())))
-                    }
-                  }
-                  .let { SystemEvents(events = it, timestamp = timestamp) }
-            }
+            .run { extractSystemEvents() }
+            .let { SystemEvents(events = it, timestamp = timestamp) }
       }
     }
   }
@@ -255,14 +204,55 @@ internal class PostgresAdapter(internal val dataSource: DataSource) : ForEventSt
           .run { executeQuery() }
           .run {
             if (next()) getString(EventSourceSql.Columns.APPEND_TOKEN)
-            else throw StreamDoesNotExist(streamName)
+            else throw StreamDoesNotExist(name = streamName)
           }
 
   override fun validateAppendToken(streamName: String, token: String): Boolean {
-    return retrieveAppendToken(streamName) == token
+    return retrieveAppendToken(streamName = streamName) == token
+  }
+}
+
+internal class EventSourceSql {
+  object Query {
+    const val INSERT_EVENT =
+        """INSERT INTO ${Tables.EVENTS} (${Columns.STREAM_NAME}, ${Columns.VERSION}, ${Columns.EVENT_ID}, ${Columns.EVENT_TYPE}, ${Columns.EVENT_DATA}, ${Columns.OCCURRED_ON})
+           VALUES                       (           ?,                      ?,                  ?,                  ?,                  (?::jsonb),                 ?         );"""
+    const val RETRIEVE_STREAM_WITH_FILTER =
+        """SELECT * FROM ${Tables.EVENTS} 
+           WHERE ${Columns.STREAM_NAME} = ?
+           AND ${Columns.EVENT_TYPE} = ANY (?)
+           ORDER BY ${Columns.VERSION};"""
+    const val RETRIEVE_STREAM =
+        """SELECT * FROM ${Tables.EVENTS}
+           WHERE ${Columns.STREAM_NAME} = ?
+           ORDER BY ${Columns.VERSION};"""
+    const val RETRIEVE_SYSTEM = "SELECT * FROM ${Tables.EVENTS} ORDER BY ${Columns.OCCURRED_ON};"
+    const val RETRIEVE_SYSTEM_WITH_FILTER =
+        """SELECT * FROM ${Tables.EVENTS}
+        WHERE ${Columns.EVENT_TYPE} = ANY (?)
+        ORDER BY ${Columns.OCCURRED_ON};"""
+    const val STREAM_EXISTS = "SELECT 1 FROM ${Tables.EVENTS} WHERE ${Columns.STREAM_NAME} = ?;"
+    const val RETRIEVE_APPEND_TOKEN =
+        "SELECT ${Columns.APPEND_TOKEN} FROM ${Tables.APPEND_TOKENS} WHERE ${Columns.STREAM_NAME} = ?;"
+    const val RETRIEVE_SYSTEM_TIMESTAMP =
+        "SELECT COALESCE(MAX(${Columns.TIMESTAMP}), CURRENT_TIMESTAMP) AS timestamp FROM ${Tables.EVENTS};"
   }
 
-  companion object
+  object Tables {
+    const val EVENTS = "eventsource.events"
+    const val APPEND_TOKENS = "eventsource.append_tokens"
+  }
+
+  object Columns {
+    const val STREAM_NAME = "stream_name"
+    const val VERSION = "version"
+    const val EVENT_ID = "event_id"
+    const val EVENT_TYPE = "event_type"
+    const val EVENT_DATA = "event_data"
+    const val OCCURRED_ON = "occurred_on"
+    const val APPEND_TOKEN = "append_token"
+    const val TIMESTAMP = "timestamp"
+  }
 }
 
 /*
@@ -270,3 +260,26 @@ Private! Utility Extension Functions
  */
 private fun Throwable.isUniqueConstraintViolation() =
     this is SQLException && this.sqlState == "23505"
+
+private fun ResultSet.extractSystemEvents(): List<SystemEvent> = buildList {
+  while (next()) {
+    add(
+        SystemEvent(
+            streamName = getString(EventSourceSql.Columns.STREAM_NAME),
+            streamEvent =
+                StreamEvent(
+                    eventType = getString(EventSourceSql.Columns.EVENT_TYPE),
+                    eventData = getString(EventSourceSql.Columns.EVENT_DATA),
+                    occurredOn = getTimestamp(EventSourceSql.Columns.OCCURRED_ON).toInstant())))
+  }
+}
+
+private fun ResultSet.extractStreamEvents(): List<StreamEvent> = buildList {
+  while (next()) {
+    add(
+        StreamEvent(
+            eventType = getString(EventSourceSql.Columns.EVENT_TYPE),
+            eventData = getString(EventSourceSql.Columns.EVENT_DATA),
+            occurredOn = getTimestamp(EventSourceSql.Columns.OCCURRED_ON).toInstant()))
+  }
+}
