94714bccc5457ab7df98d1a0c59f864d05914506
Xing Zhou <xing@xingzhou.me>
2025-03-27 10:11:20 -0400

    chore: configure spotless

--- src/main/java/me/xingzhou/projects/simple/event/store/App.java
+++ src/main/java/me/xingzhou/projects/simple/event/store/App.java
@@ -1,10 +1,8 @@
 package me.xingzhou.projects.simple.event.store;
 
-/**
- * Hello world!
- */
+/** Hello world! */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
 }
