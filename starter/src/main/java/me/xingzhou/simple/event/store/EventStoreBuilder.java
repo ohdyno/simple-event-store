40c332af90db1a8044e18d551dec2a3866c700a9
Xing Zhou <xing@xingzhou.me>
2025-11-22 21:43:30 -0500

    feat: augment starter to enable building the event store with production dependencies.

--- starter/src/main/java/me/xingzhou/simple/event/store/EventStoreBuilder.java
+++ starter/src/main/java/me/xingzhou/simple/event/store/EventStoreBuilder.java
@@ -1,19 +1,43 @@
 package me.xingzhou.simple.event.store;
 
 import java.util.Map;
+import java.util.Objects;
 import me.xingzhou.simple.event.store.enrich.EntityEventApplier;
 import me.xingzhou.simple.event.store.enrich.EventTypesExtractor;
+import me.xingzhou.simple.event.store.event.converter.EventTypeConverter;
 import me.xingzhou.simple.event.store.event.converter.MapBackedEventTypeConverter;
+import me.xingzhou.simple.event.store.event.converter.ServiceLoaderEventTypeConverter;
 import me.xingzhou.simple.event.store.serializer.adapters.JacksonEventSerializer;
+import me.xingzhou.simple.event.store.storage.EventStorage;
 import me.xingzhou.simple.event.store.storage.adapters.InMemoryEventStorage;
 
 public class EventStoreBuilder {
     public static EventStore buildWithDefaults(Map<String, Class<? extends Event>> mapping) {
-        var converter = new MapBackedEventTypeConverter(mapping);
+        return new EventStoreBuilder()
+                .withEventTypeConverter(new MapBackedEventTypeConverter(mapping))
+                .build();
+    }
+
+    private EventTypeConverter eventTypeConverter;
+
+    private EventStorage storage;
+
+    public EventStore build() {
+        var converter = Objects.requireNonNullElse(eventTypeConverter, new ServiceLoaderEventTypeConverter());
         var extractor = new EventTypesExtractor(converter);
         var serializer = new JacksonEventSerializer(converter);
-        var storage = new InMemoryEventStorage();
+        var storage = Objects.requireNonNullElse(this.storage, new InMemoryEventStorage());
         var applier = new EntityEventApplier(extractor);
         return EventStore.build(new EventStoreDependencies(storage, serializer, extractor, applier));
+    }
+
+    public EventStoreBuilder withEventTypeConverter(EventTypeConverter eventTypeConverter) {
+        this.eventTypeConverter = eventTypeConverter;
+        return this;
+    }
+
+    public EventStoreBuilder withStorage(EventStorage storage) {
+        this.storage = storage;
+        return this;
     }
 }
